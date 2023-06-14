f9d2fe0fa4182a901be27015ce456601cedff46a
RahulSelvakumar <112105461+RahulSelvakumar@users.noreply.github.com>
2023-06-14 22:58:40 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function App() {
+    const [input, setInput] = useState("");
+    const [list, setList] = useState([]);
+
+    function handleChange(event) {
+        const value = event.target.value;
+        setInput(value);
+    }
+
+    function addItem() {
+        setList(preValue => {
+            return [...preValue, input];
+        })
+        setInput("");
+    }
+
+    function deleteItem(id) {
+        console.log(id);
+    }
+
+    return (
+        <div>
+            <heading><h1>To Do List</h1></heading>
+
+            <div className="input">
+                <input placeholder="What's the task" onChange={handleChange} value={input} />
+                <span><button onClick={addItem}>+</button></span>
+            </div>
+
+            <div className="List">
+                <ul>
+                    {
+                        list.map((item, key) => { return <li onClick={()=>{deleteItem(key)}}>{item}</li> })
+                    }
+                </ul>
+            </div>
+        </div>);
+}
+export default App;
